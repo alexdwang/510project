6eb6dc3e3c186e29edf329929e967d695d81c87b
alexdwang <alexdwang@hotmail.com>
2017-03-12 18:29:11 -0700

    BTree for Edge label, Edge weight and Node label

---
+++ graphtools/EdgeLabels.java
@@ -0,0 +1,79 @@
+package graphtools;
+
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+import heap.*;
+import bufmgr.*;
+import diskmgr.*;
+import edgeheap.*;
+import global.*;
+import btree.*;
+import graphtools.*;
+
+class EdgeLabelsDriver implements GlobalConst {
+	public int keyType;
+	public BTreeFile file;
+	public HFManager hfm = null;
+
+	protected String dbpath;
+
+	EdgeLabelsDriver() {
+
+	}
+
+	EdgeLabelsDriver(HFManager hfms) {
+		this.hfm = hfms;
+	}
+
+	public void runTests() throws Exception {
+		keyType = AttrType.attrString;
+		System.out.println("start test 1!!!");
+		ConstructBTEL();
+		System.out.println("start test 2!!!");
+		BT.printBTree(file.getHeaderPage());
+		System.out.println("start test 3!!!");
+		BT.printAllLeafPages(file.getHeaderPage());
+
+	}
+
+	public void ConstructBTEL() throws Exception {
+
+		file = new BTreeFile("EdgeLabelTree", keyType, 20, 1);// full delete
+		Edge edge = new Edge();
+		KeyClass key;
+		RID rid = new RID();
+		hfm.initScanEdge();
+
+		while ((edge = hfm.scanNextEdge()) != null) {
+			key = new StringKey(edge.getLabel());
+			rid = hfm.getCurRID();
+			file.insert(key, rid);
+		}
+	}
+
+}
+
+public class EdgeLabels implements GlobalConst {
+	public static void main(String[] argvs) {
+		System.out.println("testing!!!");
+
+		EdgeLabelsDriver test = new EdgeLabelsDriver();
+		try {
+			test.runTests();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public void buildEdgeIndex(HFManager hfm) {
+		EdgeLabelsDriver test = new EdgeLabelsDriver();
+		try {
+			test.runTests();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
---
+++ graphtools/NodeLabels.java
@@ -0,0 +1,80 @@
+package graphtools;
+
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+import heap.*;
+import nodeheap.Node;
+import bufmgr.*;
+import diskmgr.*;
+import edgeheap.*;
+import global.*;
+import btree.*;
+import graphtools.*;
+
+class NodeLabelsDriver implements GlobalConst {
+	public int keyType;
+	public BTreeFile file;
+	public HFManager hfm = null;
+
+	protected String dbpath;
+
+	NodeLabelsDriver() {
+
+	}
+
+	NodeLabelsDriver(HFManager hfms) {
+		this.hfm = hfms;
+	}
+
+	public void runTests() throws Exception {
+		keyType = AttrType.attrString;
+		System.out.println("start test 1!!!");
+		ConstructBTNL();
+		System.out.println("start test 2!!!");
+		BT.printBTree(file.getHeaderPage());
+		System.out.println("start test 3!!!");
+		BT.printAllLeafPages(file.getHeaderPage());
+
+	}
+
+	public void ConstructBTNL() throws Exception {
+
+		file = new BTreeFile("NodeLabelTree", keyType, 20, 1);// full delete
+		Node node = new Node();
+		KeyClass key;
+		RID rid = new RID();
+		hfm.initScanNode();
+
+		while ((node = hfm.scanNextNode()) != null) {
+			key = new StringKey(node.getLabel());
+			rid = hfm.getCurRID();
+			file.insert(key, rid);
+		}
+	}
+
+}
+
+public class NodeLabels implements GlobalConst {
+	public static void main(String[] argvs) {
+		System.out.println("testing!!!");
+
+		EdgeLabelsDriver test = new EdgeLabelsDriver();
+		try {
+			test.runTests();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public void buildEdgeIndex(HFManager hfm) {
+		EdgeLabelsDriver test = new EdgeLabelsDriver();
+		try {
+			test.runTests();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
