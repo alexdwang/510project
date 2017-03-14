563ac90d104ec85c4680030a67775bb9f5bde4b7
Zongsi Zhang <zongsizhang@zongsis-mbp.mobile.asu.edu>
2017-03-14 04:16:52 -0700

    modify HFManager.java

--- graphtools/HFManager.java
+++ graphtools/HFManager.java
@@ -93,7 +93,7 @@ class HFManager implements  GlobalConst{
 		}
 		System.out.println(cnt + " nodes inserted");
 
-		/*
+		
 		//test scan
 		initScanNode();
 		Node node = scanNextNode();
@@ -101,7 +101,6 @@ class HFManager implements  GlobalConst{
 			node.print();
 			node = scanNextNode();
 		}
-		*/
 		
 		return true;
 	}
@@ -273,6 +272,345 @@ class HFManager implements  GlobalConst{
 		return dstSet.size();
 	}
 
+	public void NodeQuery(int qtype){
+		switch(qtype){
+		case 0:
+		{
+			initScanNode();
+			Node node = scanNextNode();
+			try{
+				while(node != null){
+					node.print();
+					node = scanNextNode();
+				}
+			}catch(Exception e){
+				System.err.println ("failed when query by label\n");
+				e.printStackTrace();
+			}
+			break;
+		}
+		case 1:
+		{
+			List<Node> nodes = new ArrayList<Node>();
+			initScanNode();
+			Node node = scanNextNode();
+			while(node != null){
+				nodes.add(node);
+				node = scanNextNode();
+			}
+			//sort
+			try{
+				Collections.sort(nodes, LabelComparator);
+				for(Node n : nodes){
+					n.print();
+				}
+			}catch(Exception e){
+				System.err.println ("failed when query by label\n");
+				e.printStackTrace();
+			}
+			break;
+		}
+
+		}
+
+	}
+
+	public void EdgeQuery01234(int qtype){
+		List<Edge> edges = new ArrayList<Edge>();
+		initScanEdge();
+		Edge edge = scanNextEdge();
+		try{
+			while(edge != null){
+				edges.add(edge);
+				edge = scanNextEdge();
+			}
+		}catch(Exception e){
+			System.err.println ("failed when query by label\n");
+			e.printStackTrace();
+		}
+		//sort
+		try{
+			if(qtype == 1) Collections.sort(edges, SourceLabelComparator);
+			else if(qtype == 2) Collections.sort(edges, DstLabelComparator);
+			else if(qtype == 3) Collections.sort(edges, EdgeLabelComparator);
+			else if(qtype == 4) Collections.sort(edges, WeightComparator);
+			for(Edge e : edges){
+				e.print();
+			}
+		}catch(Exception e){
+			System.err.println ("failed when query by label\n");
+			e.printStackTrace();
+		}
+	}
+
+	public void EdgeQuery5(int lb, int ub){
+		List<Edge> edges = new ArrayList<Edge>();
+		initScanEdge();
+		Edge edge = scanNextEdge();
+		try{
+			while(edge != null){
+				int wgt = edge.getWeight();
+				if(wgt <= ub && wgt >= lb) edges.add(edge);
+				edge = scanNextEdge();
+			}
+			for(Edge e : edges){
+				e.print();
+			}
+		}catch(Exception e){
+			System.err.println ("failed when query by label\n");
+			e.printStackTrace();
+		}
+	}
+
+	public static Comparator<Edge> SourceLabelComparator
+                          = new Comparator<Edge>() {
+
+	    public int compare(Edge n1, Edge n2) {
+	    	String l1 = "", l2 = "";
+	    	try{
+	    		l1 = n1.getSource();
+	    		l2 = n2.getSource();
+	    	}catch(Exception e){
+	    		System.err.println ("failed when query by label\n");
+				e.printStackTrace();
+	    	}
+	      //ascending order
+	      return l1.compareTo(l2);
+
+	      //descending order
+	      //return fruitName2.compareTo(fruitName1);
+	    }
+
+	};
+
+	public static Comparator<Edge> DstLabelComparator
+                          = new Comparator<Edge>() {
+
+	    public int compare(Edge n1, Edge n2) {
+	    	String l1 = "", l2 = "";
+	    	try{
+	    		l1 = n1.getDestination();
+	    		l2 = n2.getDestination();
+	    	}catch(Exception e){
+	    		System.err.println ("failed when query by label\n");
+				e.printStackTrace();
+	    	}
+	      //ascending order
+	      return l1.compareTo(l2);
+
+	      //descending order
+	      //return fruitName2.compareTo(fruitName1);
+	    }
+
+	};
+
+	public static Comparator<Edge> EdgeLabelComparator
+                          = new Comparator<Edge>() {
+
+	    public int compare(Edge n1, Edge n2) {
+	    	String l1 = "", l2 = "";
+	    	try{
+	    		l1 = n1.getLabel();
+	    		l2 = n2.getLabel();
+	    	}catch(Exception e){
+	    		System.err.println ("failed when query by label\n");
+				e.printStackTrace();
+	    	}
+	      //ascending order
+	      return l1.compareTo(l2);
+
+	      //descending order
+	      //return fruitName2.compareTo(fruitName1);
+	    }
+
+	};
+
+	public static Comparator<Edge> WeightComparator
+                          = new Comparator<Edge>() {
+
+	    public int compare(Edge e1, Edge e2) {
+	    	int l1 = 0, l2 = 0;
+	    	try{
+	    		l1 = e1.getWeight();
+	    		l2 = e2.getWeight();
+	    	}catch(Exception e){
+	    		System.err.println ("failed when query by label\n");
+				e.printStackTrace();
+	    	}
+	      //ascending order
+	      return l1 - l2;
+
+	      //descending order
+	      //return fruitName2.compareTo(fruitName1);
+	    }
+
+	};
+
+	public void NodeQuery2(Descriptor desc){
+		List<Node> nodes = new ArrayList<Node>();
+		initScanNode();
+		opt_desc.copyDesc(desc);
+		Node node = scanNextNode();
+		while(node != null){
+			nodes.add(node);
+			node = scanNextNode();
+		}
+		//sort
+		try{
+			Collections.sort(nodes, DescriptorComparator);
+			for(Node n : nodes){
+				n.print();
+			}
+		}catch(Exception e){
+			System.err.println ("failed when query by label\n");
+			e.printStackTrace();
+		}
+	}
+
+	public void NodeQuery3(Descriptor desc, double distance){
+		List<String> labels = new ArrayList<String>();
+		initScanNode();
+		opt_desc.copyDesc(desc);
+		Node node = scanNextNode();
+		while(node != null){
+			try{
+				Descriptor adesc = node.getDesc();
+				if(adesc.distance(desc) == distance){
+					String str = node.getLabel();
+					labels.add(str);
+				}
+			}catch(Exception e){
+				System.err.println ("failed when qtype = 3\n");
+				e.printStackTrace();
+			}
+			node = scanNextNode();
+		}
+		//sort
+		for(String s : labels){
+			System.out.println(s);
+		}
+	}
+
+	public void NodeQuery4(String label){
+		List<Edge> edges = new ArrayList<Edge>();
+		initScanNode();
+		Node node = scanNextNode();
+		while(node != null){
+			try{
+				String lbl = node.getLabel();
+				if(label.compareTo(lbl) == 0){
+					node.print();
+					break;
+				}
+			}catch(Exception e){
+				System.err.println ("failed when qtype = 4\n");
+				e.printStackTrace();
+			}
+			node = scanNextNode();
+		}
+
+		initScanEdge();
+		Edge edge = scanNextEdge();
+		//find edges
+		try{
+			while(edge != null){
+				String src = edge.getSource();
+				String dst = edge.getDestination();
+				if(src.compareTo(label) == 0 || dst.compareTo(label) == 0){
+					edge.print();
+				}
+				edge = scanNextEdge();
+			}
+		}catch(Exception e){
+				System.err.println ("failed when qtype = 3\n");
+				e.printStackTrace();
+		}
+	}
+
+	public void NodeQuery5(Descriptor desc, double distance){
+		List<Node> labels = new ArrayList<Node>();
+		initScanNode();
+		opt_desc.copyDesc(desc);
+		Node node = scanNextNode();
+		while(node != null){
+			try{
+				Descriptor adesc = node.getDesc();
+				if(adesc.distance(desc) == distance){
+					labels.add(node);
+				}
+			}catch(Exception e){
+				System.err.println ("failed when qtype = 5\n");
+				e.printStackTrace();
+			}
+			node = scanNextNode();
+		}
+
+		initScanEdge();
+		Edge edge = scanNextEdge();
+		//find edges
+		try{
+			for(Node n : labels){
+				n.print();
+				String label = n.getLabel();
+				while(edge != null){
+					String src = edge.getSource();
+					String dst = edge.getDestination();
+					if(src.compareTo(label) == 0 || dst.compareTo(label) == 0){
+						edge.print();
+					}
+					edge = scanNextEdge();
+				}
+			}
+		}catch(Exception e){
+				System.err.println ("failed when qtype = 5\n");
+				e.printStackTrace();
+		}
+	}
+
+	public static Comparator<Node> LabelComparator
+                          = new Comparator<Node>() {
+
+	    public int compare(Node n1, Node n2) {
+	    	String l1 = "", l2 = "";
+	    	try{
+	    		l1 = n1.getLabel();
+	    		l2 = n2.getLabel();
+	    	}catch(Exception e){
+	    		System.err.println ("failed when query by label\n");
+				e.printStackTrace();
+	    	}
+	      //ascending order
+	      return l1.compareTo(l2);
+
+	      //descending order
+	      //return fruitName2.compareTo(fruitName1);
+	    }
+
+	};
+
+	public static Descriptor opt_desc = new Descriptor();
+	public static Comparator<Node> DescriptorComparator
+                          = new Comparator<Node>() {
+
+	    public int compare(Node n1, Node n2) {
+	    	Descriptor l1 = new Descriptor();
+	    	Descriptor l2 = new Descriptor();
+	    	try{
+	    		l1.copyDesc(n1.getDesc());
+	    		l2.copyDesc(n2.getDesc());
+	    	}catch(Exception e){
+	    		System.err.println ("failed when query by label\n");
+				e.printStackTrace();
+	    	}
+	      //ascending order
+	      return (int)Math.round(l1.distance(opt_desc) - l2.distance(opt_desc));
+
+	      //descending order
+	      //return fruitName2.compareTo(fruitName1);
+	    }
+
+	};
+
 	public int getLabelCnt()
 	throws IOException,
 	FieldNumberOutOfBoundException
@@ -294,6 +632,9 @@ class HFManager implements  GlobalConst{
 		}
 		return lblSet.size();
 	}
+
+
+
 	
 	public boolean deletenode(RID rid)
 			throws Exception
@@ -303,7 +644,7 @@ class HFManager implements  GlobalConst{
 		return true;
 		
 	}
-	
+
 	public boolean deleteedge(RID rid)
 			throws Exception
 	{
